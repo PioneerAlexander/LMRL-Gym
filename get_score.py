df4d87a80781fcb86de6d9c94589eaff9dd82bf7
PioneerAlexander <karjakin03@gmail.com>
2024-04-02 12:52:25 +0000

    log score statistics in get_score.py function

--- get_score.py
+++ get_score.py
@@ -29,23 +29,41 @@ def main(
     """
     if tau is None:
         file_path = os.path.join(
-                dir_path,
-                str(seed),
-                "results.json",
-                )
+            dir_path,
+            str(seed),
+            "results.json",
+        )
     else:
         file_path = os.path.join(
-                dir_path,
-                str(seed),
-                str(beta),
-                str(tau),
-                "results.json",
-                )
-    with open(file_path, "r", encoding="utf-8") as f:
-        results = json.load(f)["reward_eval"].values()
+            dir_path,
+            str(seed),
+            str(beta),
+            str(tau),
+            "results.json",
+        )
+    with open(file_path, "r", encoding="utf-8") as eval_results:
+        results = json.load(eval_results)["reward_eval"].values()
     rewards = [info["reward"]["mean"] for info in results]
     raw_reward = np.mean(rewards)
     print(f"Raw average reward maze: {raw_reward}")
+
+    result = {
+        "seed": seed,
+        "beta": beta,
+        "tau": tau,
+        "score": raw_reward,
+    }
+    results_filename = "iql_results.json"
+
+    try:
+        with open(results_filename, "r", encoding="utf-8") as log_score:
+            content = json.load(log_score)
+    except FileNotFoundError:
+        content = []
+    content.append(result)
+
+    with open(results_filename, "w", encoding="utf-8") as log_score:
+        json.dump(content, log_score, indent=4)
 
 
 if __name__ == "__main__":
